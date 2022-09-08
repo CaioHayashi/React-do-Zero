37267879769ccebaf1c9a3ba6b5d6f96b8132b44
CaioHayashi <84331740+CaioHayashi@users.noreply.github.com>
2022-09-07 22:18:47 -0300

    secret word - finished

--- secret-word/src/components/Game.js
+++ secret-word/src/components/Game.js
@@ -1,12 +1,74 @@
-import "./Game.css"
+import { useRef, useState } from "react";
+import "./Game.css";
 
-const Game = ({ verifyLetter }) => {
-  return (
-    <div>
-        <h1>Game</h1>
-        <button onClick={ verifyLetter }>Finalizar Jogo</button>
-    </div>
-  )
-}
+const Game = ({
+	verifyLetter,
+	pickedWord,
+	pickedCategory,
+	letters,
+	guessedLetters,
+	wrongLetters,
+	guesses,
+	score
+}) => {
+	const [letter, setLetter] = useState("");
+	const letterInputRef = useRef(null);
 
-export default Game
+	const handleSubmit = (e) => {
+		e.preventDefault();
+
+		verifyLetter(letter);
+		setLetter("");
+
+		letterInputRef.current.focus();
+	};
+
+	return (
+		<div className="game">
+			<p className="points">
+				<span>Pontuação: {score}</span>
+			</p>
+			<h1>Advinhe a palavra:</h1>
+			<h3 className="tip">
+				Dica sobre a palavra: <span>{pickedCategory}</span>
+			</h3>
+			<p>Você ainda tem {guesses} tentativa(s).</p>
+			<div className="wordContainer">
+				{letters.map((letter, i) =>
+					guessedLetters.includes(letter) ? (
+						<span key={i} className="letter">
+							{letter}
+						</span>
+					) : (
+						<span key={i} className="blankSquare"></span>
+					)
+				)}
+			</div>
+			<div className="letterContainer">
+				<p>Tente advinhar uma letra da palavra:</p>
+				<form onSubmit={handleSubmit}>
+					<input
+						type="text"
+						name="letter"
+						maxLength="1"
+						required
+						onChange={(e) => setLetter(e.target.value)}
+						value={letter}
+						ref={letterInputRef}
+					/>
+					<button>Jogar!</button>
+				</form>
+			</div>
+			<div className="wrongLettersContainer">
+				<p>Letras já utilizadas:</p>
+				<span>
+					{wrongLetters.map((letter, i) => (
+						<span key={i}>{letter.toUpperCase()}, </span>
+					))}
+				</span>
+			</div>
+		</div>
+	);
+};
+
+export default Game;
--- secret-word/src/components/GameOver.js
+++ secret-word/src/components/GameOver.js
@@ -1,12 +1,15 @@
 import "./GameOver.css";
 
-const GameOver = ({ retryGame }) => {
-  return (
-    <div>
-      <h1>GameOver</h1>
-      <button onClick={retryGame}>Resetar Jogo</button>
-    </div>
-  );
+const GameOver = ({ retryGame, score }) => {
+	return (
+		<div>
+			<h1>Fim de Jogo!</h1>
+			<h2>
+				A sua pontuação foi: <span>{score}</span>
+			</h2>
+			<button onClick={retryGame}>Resetar Jogo</button>
+		</div>
+	);
 };
 
 export default GameOver;
